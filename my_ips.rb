25ec817ce81ec2cef24f6d3b1fc8eb7417f261ee
gagegolden00 <malcolm.golden0@gmail.com>
2023-08-27 18:25:25 -0500

    added function to filter ifconfig for ips

--- my_ips.rb
+++ my_ips.rb
@@ -1,10 +1,30 @@
-#!/bin/env ruby
-def get_my_ips
-    public_ip = `curl ifconfig.me`
-    private_ip = `hostname -I | cut -d' ' -f1`
-    puts "
-        Public IP Address: #{public_ip}
-        Private IP Address: #{private_ip}
-    "
-end
-get_my_ips
+def extract_ip_addresses(output)
+    ip_addresses = []
+
+    lines = output.split("\n")
+    current_label = nil
+
+    lines.each do |line|
+      if line =~ /^([\w\d]+):\s/
+        current_label = $1
+      elsif line =~ /\binet (\d+\.\d+\.\d+\.\d+)/
+        ip_addresses << [current_label, $1]
+      end
+    end
+
+    ip_addresses
+  end
+
+  def get_my_ips
+    public_ip = `curl ifconfig.me`.strip
+    puts "Public IP Address: #{public_ip}"
+  end
+
+  ifconfig_output = `ifconfig`
+  ip_addresses = extract_ip_addresses(ifconfig_output)
+
+  ip_addresses.each do |(label, ip)|
+    puts "Label: #{label}, IP: #{ip}" if ip
+  end
+
+  get_my_ips
