1155d8a46545eace41f3b6f9457338bf914ceacb
gagegolden00 <malcolm.golden0@gmail.com>
2023-08-27 19:06:33 -0500

    formatted

--- my_ips.rb
+++ my_ips.rb
@@ -1,37 +1,37 @@
 def extract_ip_addresses(output)
-    ip_addresses = []
+  ip_addresses = []
 
-    lines = output.split("\n")
-    current_label = nil
+  lines = output.split("\n")
+  current_label = nil
 
-    lines.each do |line|
-      if line =~ /^([\w\d]+):\s/
-        current_label = $1
-      elsif line =~ /\binet (\d+\.\d+\.\d+\.\d+)/
-        ip_addresses << [current_label, $1]
-      end
+  lines.each do |line|
+    if line =~ /^([\w\d]+):\s/
+      current_label = Regexp.last_match(1)
+    elsif line =~ /\binet (\d+\.\d+\.\d+\.\d+)/
+      ip_addresses << [current_label, Regexp.last_match(1)]
     end
-
-    ip_addresses
-end
-
-  def get_my_ips
-    public_ip = `curl -s ifconfig.me`.strip
-    puts "--- Public IP Address---
-      #{public_ip}"
   end
 
-  def output
+  ip_addresses
+end
+
+def get_my_ips
+  public_ip = `curl -s ifconfig.me`.strip
+  puts "--- Public IP Address---
+      #{public_ip}"
+end
+
+def output
   ifconfig_output = `ifconfig`
   ip_addresses = extract_ip_addresses(ifconfig_output)
 
-  puts "--- Local IP Address --- "
+  puts '--- Local IP Address --- '
   ip_addresses.each do |(label, ip)|
-    puts "-----------------------------------------"
+    puts '-----------------------------------------'
     puts "adapter: #{label}, IPv4: #{ip}" if ip
   end
-  puts "-----------------------------------------"
+  puts '-----------------------------------------'
   get_my_ips
-  end
+end
 
 output
